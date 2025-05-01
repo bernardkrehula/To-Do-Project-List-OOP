e2becae0ec85468a471dd0607b62ea53e9d01487
bernardkrehula <krehula.bernard456@gmail.com>
2025-05-01 11:43:25 +0200

    Add isChecked in object

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 const newToDoBtn = document.querySelector('.newToDoBtn');
 const toDoTaskInput = document.querySelector('.toDoInput');
 const toDoInputDate = document.querySelector('.toDoInputDate');
-const addBtn = document.querySelector('.addBtn');
+const cancelBtn = document.querySelector('.cancelBtn');
 const newProjectBtn = document.querySelector('.newProjectBtn');
 const projects = document.querySelector('.projects');
+const projectInput = document.querySelector('.newProject input');
+const projectAddBtn = document.querySelector('.projectAddBtn');
+const projectCreator = document.querySelector('.projectCreator');
+const newToDoTask = document.querySelector('.newToDoTask');
+const newToDoFrom = document.querySelector('.newToDoForm');
 
 let toDo;
 let project;
 
-addBtn.addEventListener('click', () => {
-    const id = crypto.randomUUID();
-    toDo = new NewToDo(id, toDoTaskInput.value, toDoInputDate.value);
-    toDoTaskInput.value = '';
-    toDoInputDate.value = '';
-})
 newProjectBtn.addEventListener('click', () => {
-    project = new NewProject();
+    projectCreator.style.display = 'block';
+});
+projectAddBtn.addEventListener('click', () => {
+    const id = crypto.randomUUID();
+    project = new NewProject(projectInput.value, id);
     project.pushToDoInProjectArray(toDo);
     project.createProjectOnScreen(toDo);
     manager.pushProjectInArray(project);
-    console.log(project)
-    console.log(manager)
+})
+projects.addEventListener('click', (e) => {
+    const getId = e.target.closest('li').id;
+    const findProject = e.target.closest('li');
+    const removeBtn = e.target.closest('button');
     
-});
+    if(findProject){
+        manager.findClickedProject(getId)
+    }
+    if(removeBtn){
+        manager.removeClickedProject(getId);
+        projects.removeChild(findProject);
+    }
+})
+newToDoBtn.addEventListener('click', () => newToDoFrom.style.display = 'block');
+
+newToDoFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = crypto.randomUUID();
+    const isChecked = false;
+
+    toDo = new NewToDo(id, toDoTaskInput.value, toDoInputDate.value, isChecked);
+    toDoTaskInput.value = '';
+    toDoInputDate.value = '';
+    toDo.createNewToDoOnClick();
+    newToDoFrom.style.display = 'none';
+})
+cancelBtn.addEventListener('click', () => newToDoFrom.style.display = 'none');
--- toDo.js
+++ toDo.js
@@ -1,24 +1,35 @@
 class NewToDo {
-    constructor(id, task, date){
+    constructor(id, task, date, isChecked){
         this.id = id;
         this.task = task;
         this.date = date;
+        this.isChecked = isChecked;
+    }
+    createNewToDoOnClick(){
+        let html = `
+        <li id="${this.id}"><h3>Title: ${this.task}</h3><h4>Due date: ${this.date}</h4><button class="editBtn">edit</button><button class="deleteBtn">delete</button><input type="checkbox"></li>
+        `;
+        newToDoTask.insertAdjacentHTML('beforeend', html);
     }
 }
 
 class NewProject {
-    constructor(){
+    constructor(projectName, id){
         this.projectArray = [];
+        this.id = id;
+        this.projectName = projectName;
+        this.isClicked = false;
     }
     pushToDoInProjectArray(toDo){
         this.projectArray.push(toDo);
     }
-    createProjectOnScreen(toDo){
+    createProjectOnScreen(){
         let html = `
-        <li id="${toDo.id}"><p>${toDo.task}</p><button>x</button></li>
+        <li id="${this.id}"><p>${this.projectName}</p><button>x</button></li>
         `;
         projects.insertAdjacentHTML('beforeend', html);
     }
+ 
 }
 class NewProjectManager {
     constructor(){
@@ -26,6 +37,12 @@ class NewProjectManager {
     }
     pushProjectInArray(project){
         this.newProjectsArray.push(project);
+    }
+    findClickedProject(getId){
+        return this.newProjectsArray.find(project => project.id == getId);
+    }
+    removeClickedProject(getId){
+        this.newProjectsArray = this.newProjectsArray.filter(project => project.id != getId)
     }
 }
 const manager = new NewProjectManager();
